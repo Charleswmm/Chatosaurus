24d334f3fc72e6cf65f997634bfd17dec24c770c
Charles <charleswm@gmail.com>
2020-05-05 16:54:43 +1200

    Added babel and react to the project

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import '../scss/app.scss';
+import Top from "./components/text";
 
+
+ReactDOM.render(<Top />, document.getElementById("container"));
 
 console.log('app.js is working, ok');
