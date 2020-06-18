d93835f17ff8618377c73c297e8e812256e88b67
Charles <charleswm@gmail.com>
2020-06-18 14:39:15 +1200

    CHAT-87 Dump components into the kitchen sink

--- src/js/config/kitchensink.js
+++ src/js/config/kitchensink.js
@@ -6,19 +6,30 @@ export const configuration = {
       id: 'home',
       title: 'Home',
       iconClassName: iconClassNames.home,
-      channelExtraClassNames: [ uiClassNames.blue, uiClassNames.separator ]
+      channelExtraClassNames: [ uiClassNames.blue, uiClassNames.separator ],
+      sort: 0,
     },
     {
       id: 'green',
       title: 'This is a green button',
-      iconClassName: iconClassNames.add,
-      channelExtraClassNames: [ uiClassNames.green ]
+      imageSrc: 'url',
+      channelExtraClassNames: [ uiClassNames.green ],
+      sort: 1,
     },
     {
       id: 'blue',
       title: 'This is a blue button',
-      iconClassName: iconClassNames.add,
-      channelExtraClassNames: [ uiClassNames.blue ]
+      imageSrc: 'url',
+      channelExtraClassNames: [ uiClassNames.blue ],
+      sort: 2,
     },
-  ]
+  ],
+  addServerButton:
+    {
+      id: 'add-a-server',
+      title: 'Add a Server',
+      iconClassName: iconClassNames.add,
+      channelExtraClassNames: [ uiClassNames.green ],
+      sort: 0,
+    },
 }
--- src/js/kitchensink.js
+++ src/js/kitchensink.js
@@ -4,17 +4,28 @@ import '../scss/app.scss';
 import Chat from "./components/Chat/Chat";
 import MainNav from "./components/MainNav/MainNav";
 import GroupNav from "./components/GroupNav/GroupNav";
-import { GlobalContextWrapper } from "./contexts/GlobalContextWrapper";
+import AddServerButton from "./components/AddServerButton/AddServerButton";
+import MainNavButton from "./components/MainNavButton/MainNavButton";
 import Config from "./utilities/Config";
 import { configuration } from "./config/kitchensink";
+import { GlobalContextWrapper } from "./contexts/GlobalContextWrapper";
+
+const placeholderConfig = new Config(configuration);
 
 class KitchenSink extends Component {
   render () {
+  const { mainNavButtons } = placeholderConfig.get(['mainNavButtons']);
+  const { addServerButton } = placeholderConfig.get(['addServerButton']);
+
     return (
       <GlobalContextWrapper initialConfig={new Config(configuration)} >
         <nav className="nav-side">
           <MainNav />
+          <GroupNav />
         </nav>
+        <MainNavButton { ...mainNavButtons[0] }/>
+        <AddServerButton { ...addServerButton } />
+        <Chat />
       </GlobalContextWrapper>
     );
   };
