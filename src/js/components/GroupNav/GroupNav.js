6510866e9866eb72ca6dadac20176140c580818d
Charles <59349751+Charleswmm@users.noreply.github.com>
2020-06-22 16:45:13 +1200

    CHAT-42 Add placeholders to secondary menu

--- src/js/components/GroupNav/GroupNav.js
+++ src/js/components/GroupNav/GroupNav.js
@@ -7,20 +7,26 @@ class GroupNav extends Component {
     return(
       <div className="nav-column nav-column-message">
         <div className="nav-group nav-group-top">
-          <div className="nav-item nav-item-start">
-            <button className="btn btn-nav-item-start">Find or start a conversation</button>
+          <div className="nav-item nav-item-top">
+            <button className="nav-btn-top">Find or start a conversation</button>
           </div>
         </div>
-        <div className="nav-group nav-group-dm flex-grow">
+        <div className="nav-group nav-group-dm">
           <div className="nav-subgroup nav-subgroup-menu">
             <div className="nav-item nav-item-menu">
-              <button className="btn btn-nav-item">
-                <div className="btn-nav-item-text">Friends</div>
+              <button className="nav-btn">
+                <div className="btn-content">
+                  <div className="svg svg-friend"/>
+                  <div className="btn-text">Friends</div>
+                </div>
               </button>
             </div>
             <div className="nav-item nav-item-menu">
-              <button className="btn btn-nav-item">
-                <div className="btn-nav-item-text">Nitro</div>
+              <button className="nav-btn">
+                <div className="btn-content">
+                  <div className="svg svg-nitro"/>
+                  <div className="btn-text">Nitro</div>
+                </div>
               </button>
             </div>
           </div>
