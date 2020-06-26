3792ad20af7302d1799148ea02d9231f506bd9d6
Charles <charleswm@gmail.com>
2020-06-26 17:24:26 +1200

    CHAT-104 Add and setup react-router

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
-import React, { Component }  from "react";
+import React from "react";
 import ReactDOM from 'react-dom';
 import '../scss/app.scss';
 import Chat from "./components/Chat/Chat";
 import MainNav from "./components/MainNav/MainNav";
 import GroupNav from "./components/GroupNav/GroupNav";
 import { GlobalContextWrapper } from "./contexts/GlobalContextWrapper";
+import { BrowserRouter } from "react-router-dom";
 import Config from "./utilities/Config";
 import { configuration } from "./config/app";
 
 const App = () => {
   return (
+    <BrowserRouter>
       <GlobalContextWrapper Config={new Config(configuration)} >
         <nav className="nav-side">
           <MainNav />
@@ -17,6 +19,7 @@ const App = () => {
         </nav>
         <Chat />
       </GlobalContextWrapper>
+    </BrowserRouter>
   );
 };
 
