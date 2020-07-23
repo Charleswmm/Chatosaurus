de6d997c63cd32f575d6e7693dfbf8ef6ce20b3d
Charles <charleswm@gmail.com>
2020-07-23 17:45:54 +1200

    CHAT-156 Create Landing Page

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ import '../scss/app.scss';
 import ChatControl from './components/ChatControl/ChatControl';
 import Friends from './components/Friends/Friends';
 import GroupNav from './components/GroupNav/GroupNav';
+import LandingPage from './components/LandingPage/LandingPage';
 import MainNav from './components/MainNav/MainNav';
 import configuration from './config/app';
 import { GlobalContextWrapper } from './contexts/GlobalContextWrapper';
@@ -13,17 +14,20 @@ import Config from './utilities/Config';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path={['/', '/channels']} render={() => <Redirect to="/channels/@me" />} />
-    <GlobalContextWrapper Config={new Config(configuration)}>
-      <MainNav />
-      <Route path="/channels/@me">
-        <GroupNav />
-        <Switch>
-          <Route path="/channels/@me/:id" component={ChatControl} />
-          <Route component={Friends} />
-        </Switch>
-      </Route>
-    </GlobalContextWrapper>
+    <Switch>
+      <Route path="/" component={LandingPage} />
+      <Route exact path={['/', '/channels']} render={() => <Redirect to="/channels/@me" />} />
+      <GlobalContextWrapper Config={new Config(configuration)}>
+        <MainNav />
+        <Route path="/channels/@me">
+          <GroupNav />
+          <Switch>
+            <Route path="/channels/@me/:id" component={ChatControl} />
+            <Route component={Friends} />
+          </Switch>
+        </Route>
+      </GlobalContextWrapper>
+    </Switch>
   </BrowserRouter>
 );
 
