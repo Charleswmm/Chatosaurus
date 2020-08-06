3ebd90458c24f56d50c114ca949123caaebcd412
Charles <charleswm@gmail.com>
2020-08-07 08:55:35 +1200

    CHAT-177 Unit Tests

--- src/js/utilities/tests/DiscordStore.test.js
+++ src/js/utilities/tests/DiscordStore.test.js
@@ -1,21 +1,45 @@
 import axios from 'axios';
+import Config from '../Config';
 import DiscordStore from '../DiscordStore';
 
 jest.mock('axios');
 
 describe('DiscordStore', () => {
-  const mockData = {
-    foo: 'foo',
-    bar: 'bar',
+  const fooConfiguration = {
+    discordUrls: '',
+    discordAPIResources: {
+      client: 'bar',
+    },
+    tokenTemplate: '',
   };
 
-  it('constructs', () => {
-    const discordStore = new DiscordStore(mockData);
+  const fooConfig = new Config(fooConfiguration);
+  const discordStore = new DiscordStore(fooConfig);
 
+  const bin = {
+    access_token: 'foo',
+    token_type: 'bar',
+  };
+
+  const getItemMock = () => JSON.stringify(bin);
+
+  const sessionStorageMock = {
+    getItem: getItemMock,
+  };
+
+  Object.defineProperty(window, 'sessionStorage', {
+    value: sessionStorageMock,
+  });
+
+  axios.get.mockImplementation(() => Promise.resolve({
+    data: 'bin',
+  }));
+
+  it('constructs', () => {
     expect(discordStore).toBeInstanceOf(DiscordStore);
   });
 
-  it('can get user data', () => {
-
-  });
+  it('can get data', () => expect(
+    discordStore.getData('foo', 'bar').then((e) => e),
+  ).resolves.toBe('bin'));
 });
